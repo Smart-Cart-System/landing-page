26a493b94a8d517b7d1717423f471a7cbbe56536
Zafaranii <marwan.elzafarani@gmail.com>
2025-05-05 00:36:17 +0300

    Fix  path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HeroSection from './components/HeroSection';
 import ProblemSection from './components/ProblemSection';
 import FeaturesSection from './components/FeaturesSection';
